5aebadd92deaa157eb738e93d299dfdd415c2420
Thysrael <2470583476@qq.com>
2023-05-25 11:10:41 +0800

    qs-增加 mkdir-p

--- kernel/entry/init.c
+++ kernel/entry/init.c
@@ -74,7 +74,7 @@ void main(u64 hartId)
     plicinithart();
 
     PROCESS_CREATE_PRIORITY(processA, 1);
-    PROCESS_CREATE_PRIORITY(processB, 1);
+    PROCESS_CREATE_PRIORITY(processC, 1);
     // PROCESS_CREATE_PRIORITY(processC, 1);
 
     yield();
