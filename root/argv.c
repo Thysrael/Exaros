61a40729e96a5246b361349786f13d8e05dd3a41
chlience <chlience@outlook.com>
2023-08-18 15:54:51 +0800

    style(gcc): 修改文件结构

---
+++ root/argv.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    int a, b;
+    FILE *file = fopen("argv.out", "w");
+    if (file == NULL)
+    {
+        printf("No such file!\n");
+        exit(0);
+    }
+    fprintf(file, "argc=%d\n", argc);
+    for (int i = 0; i < argc; i++)
+    {
+        fprintf(file, "argv[%d]: %s\n", i, argv[i]);
+    }
+    fclose(file);
+    return 0;
+}
